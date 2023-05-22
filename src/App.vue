2c1e048371c292b5877ccff37cca381e9968d688
CsalaPeter <csalapeter95@gmail.com>
2023-05-22 11:33:48 +0200

    Basic functionality finished

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,80 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted, computed, watch } from "vue";
+
+const todos = ref([]);
+const inputContent = ref("");
+const todosAsc = computed(() =>
+  todos.value.sort((a, b) => {
+    return a.createdAt - b.createdAt;
+  })
+);
+
+watch(
+  todos,
+  (newVal) => {
+    localStorage.setItem("todos", JSON.stringify(newVal));
+  },
+  {
+    deep: true,
+  }
+);
+
+const addTodo = () => {
+  if (inputContent.value.trim() === "") {
+    return;
+  }
+  todos.value.push({
+    content: inputContent.value,
+    done: false,
+    createdAt: new Date().getTime(),
+  });
+  inputContent.value = "";
+};
+
+const removeTodo = (todo) => {
+  todos.value = todos.value.filter((t) => t !== todo);
+};
+
+onMounted(() => {
+  todos.value = JSON.parse(localStorage.getItem("todos")) || [];
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <main class="app">
+    <section class="header">
+      <h2 class="title">Greetings User!</h2>
+    </section>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+    <section class="createTodo">
+      <h3>Add todo to your list!</h3>
+      <form @submit.prevent="addTodo">
+        <input
+          type="text"
+          placeholder="New todo..."
+          name="content"
+          id="content"
+          v-model="inputContent"
+        />
+        <input type="submit" value="Add todo" />
+      </form>
+    </section>
+
+    <section class="todoList">
+      <h3>Todo List</h3>
+      <div class="list">
+        <div v-for="todo in todosAsc" :class="todoItem">
+          <label>
+            <input type="checkbox" v-model="todo.done" />
+          </label>
+          <div class="todo-content">
+            <input type="text" v-model="todo.content" />
+          </div>
+          <div class="actions">
+            <button class="delete" @click="removeTodo(todo)">Delete</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
