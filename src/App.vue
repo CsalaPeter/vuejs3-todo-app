b438fbb0c30f70507212ea3dac3e8a42d1bab2a8
CsalaPeter <csalapeter95@gmail.com>
2023-05-26 19:21:59 +0200

    Css makeover

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import { ref, onMounted, computed, watch } from "vue";
 
+const newLocal = "bubble business";
 const todos = ref([]);
 const inputContent = ref("");
+const inputCategory = ref(null);
 const todosAsc = computed(() =>
   todos.value.sort((a, b) => {
     return a.createdAt - b.createdAt;
@@ -25,6 +27,7 @@ const addTodo = () => {
   }
   todos.value.push({
     content: inputContent.value,
+    category: inputCategory.value,
     done: false,
     createdAt: new Date().getTime(),
   });
@@ -43,12 +46,46 @@ onMounted(() => {
 <template>
   <main class="app">
     <section class="header">
-      <h2 class="title">Your Todo List</h2>
+      <h2 class="title">Your To-Do List</h2>
     </section>
 
     <div class="todo-body">
+      <section class="todo-list">
+        <h3>To-Do's</h3>
+        <div class="list">
+          <div
+            v-for="todo in todosAsc"
+            :class="`todo-item ${todo.done && 'done'}`"
+          >
+            <div class="icon-container">
+              <v-icon
+                v-if="todo.category === 'work'"
+                class="work-icon"
+                name="md-work"
+              />
+              <v-icon
+                v-else-if="todo.category === 'personal'"
+                class="person-icon"
+                name="io-person"
+              />
+            </div>
+            <label>
+              <input type="checkbox" v-model="todo.done" />
+              <span class="bubble"></span>
+            </label>
+            <div class="todo-content">
+              <span class="todo-text" type="text">{{ todo.content }}</span>
+            </div>
+
+            <div class="actions">
+              <button class="delete" @click="removeTodo(todo)">Delete</button>
+            </div>
+          </div>
+        </div>
+      </section>
+
       <section class="create-todo">
-        <h3>Add todo to your list!</h3>
+        <h3>Add To-Do to your list!</h3>
         <form @submit.prevent="addTodo">
           <input
             type="text"
@@ -57,27 +94,41 @@ onMounted(() => {
             id="content"
             v-model="inputContent"
           />
+          <h4>Pick a category!</h4>
+          <div class="options">
+            <label>
+              <input
+                type="radio"
+                name="category"
+                id="category1"
+                value="work"
+                v-model="inputCategory"
+              />
+              <span class="bubble work"></span>
+              <div>
+                Work
+                <v-icon class="work-icon" name="md-work" />
+              </div>
+            </label>
+
+            <label>
+              <input
+                type="radio"
+                name="category"
+                id="category2"
+                value="personal"
+                v-model="inputCategory"
+              />
+              <span class="bubble personal"></span>
+              <div>
+                Personal <v-icon class="person-icon" name="io-person" />
+              </div>
+            </label>
+          </div>
           <div class="form-actions">
             <button class="add-button" type="submit">Add todo</button>
           </div>
         </form>
-      </section>
-
-      <section class="todo-list">
-        <h3>Todo List</h3>
-        <div class="list">
-          <div class="todo-item" v-for="todo in todosAsc" :class="todoItem">
-            <label class="todo-done">
-              <input type="checkbox" v-model="todo.done" />
-            </label>
-            <div class="todo-content">
-              <span class="todo-text" type="text">{{ todo.content }}</span>
-            </div>
-            <div class="actions">
-              <button class="delete" @click="removeTodo(todo)">Delete</button>
-            </div>
-          </div>
-        </div>
       </section>
     </div>
   </main>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { MdWork } from "oh-vue-icons/icons/md";
+import { IoPerson } from "oh-vue-icons/icons/io";
 
-createApp(App).mount('#app')
+addIcons(MdWork, IoPerson);
+
+const app = createApp(App);
+
+app.component("v-icon", OhVueIcon);
+app.mount("#app");
